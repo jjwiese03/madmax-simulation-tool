7feb3493c0025c3caf0e25434d33b15d3c40bc5e
jjwiese03 <janwiesmann03@gmail.com>
2026-07-03 16:43:05 +0200

    fixed linking issue in backend

--- backend/startup.jl
+++ backend/startup.jl
@@ -6,7 +6,7 @@ Genie.config.run_as_server = false
 Genie.config.server_host = "127.0.0.1"
 Genie.config.server_port = 8000
 Genie.config.websockets_server = true # enable the websockets server
-Genie.config.server_document_root = normpath(joinpath(@__DIR__, ".."))
+Genie.config.server_document_root = normpath(joinpath(@__DIR__, "..", "frontend"))
 Genie.config.server_handle_static_files = true
 
 
