b65a05efe0c855b4dab25065f8b65e29c46d00b6
jjwiese03 <janwiesmann03@gmail.com>
2026-07-06 00:44:31 +0200

    docs

--- docs/build-docs.bat
+++ docs/build-docs.bat
@@ -1,3 +1,2 @@
 @echo off
-cd ../
-documentation build frontend/src/DiscPlot/discplot.js -f html -o docs/DiscPlotAPI             
+documentation build ../frontend/src/DiscPlot/discplot.js -f html -o DiscPlotAPI             
